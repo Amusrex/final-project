40b7951338bab221c8d55be99f792c020f9114e7
harrisonyounger <57718444+Amusrex@users.noreply.github.com>
2023-04-25 06:38:13 +0000

    first monster done

--- Actions.py
+++ Actions.py
@@ -13,46 +13,85 @@ class actions():
 	def __init__(self):
 		self.location = array([0,0,0])
 		self.allowed_movement = []
-		self.RoomItem = ['map','note']
+		self.RoomItem = ['map','note','riddle','protective amulet','flash light','torch']
+		self.animals = ['tree monster']
 		self.player_inventory = []
+		self.actions = ['Q','S','M','I','O']
 	def move_player(self):
-		print(f"{self.allowed_movement}")
-		sdirection = input("Which way do you want to move?\n")
+		print(f"{self.actions}")
+		print(f"Allowed movements\n{self.allowed_movement}")
+		
+		sdirection = input("What would you like to do?\n")
 		direction = sdirection.upper()
 		if direction == 'Q':
 			print('Goodbye')
 			quit()
-		if direction == 'M':
+		elif direction == 'M':
 			print(f'{b.menu1()}')
 			print(f'{self.allowed_movement}')
 			sdirection = input("Which way do you want to move?\n")
-		if direction == 'I':
-			print('Here is your inventory')
-			print(f'{b.menu1()}')
+		elif direction == 'I':
+			if not self.player_inventory:
+				print('Your inventory is empty. Explore to find and gather items.')
+			else:
+				print('Here is your inventory')
+				print(f'{self.player_inventory}')
 			direction = 'N'
+		elif direction == 'O':
+			ask = input("What item would you like to use\n")
+			if ask == 'map':
+				print('''
+                         _
+             _   _   _  |_|	
+           _|_|_|_|_|_| |_|	
+          |_|_|_|_|_|_|_|_|_ _ _
+          |_| |_|   |_|_|_|_|_|_|  _        
+                   _ _ _ _ _ _|_|_|_|_
+               _ _|_|_|_|_|_|_|_|_|_|_|
+     _ 	      |_|_|_|       |_|   |_|
+    |_|_ _ _ _ _ _|_|_  
+    |_|_|_|_|_|_|_|_|_|	
+ _ _ _|_|_	
+|_|_|_|_|_|		
+  |_|_ _ _ _
+  |_|_|_|_|_|  _
+	|_|_ _|_|
+	|_|_|_|_|
+	 _|_|_
+	|_|_|_|''')
+			elif ask == 'riddle':
+				print('''In a fiery tomb, the treasure lies,
+Amidst the ash and smoke that rise.
+The earth may shake, the lava flow,
+But riches beyond measure, it will show.
+Only the bravest, the cleverest too,
+Can find the way to this hidden loot.''')
+			else:
+				print('Cannot use that item')
+
 	
 			
 			direction = sdirection.upper()
-		if direction in self.allowed_movement:
+		elif direction in self.allowed_movement:
 			self.location += MOVEMENT[direction]
 			if direction == 'N':
 				print('')
-			if direction == "U":
+			elif direction == "U":
 				print(f"You moved up")
 				print(f'{self.location}')
-			if direction == "L":
+			elif direction == "L":
 				print("You moved left")
 				print(f'{self.location}')
-			if direction == "D":
+			elif direction == "D":
 				print("You moved down")
 				print(f'{self.location}')
-			if direction == "R":
+			elif direction == "R":
 				print("You moved right")
 				print(f'{self.location}')
-			if direction == "F":
+			elif direction == "F":
 				print("You moved forward")
 				print(f'{self.location}')
-			if direction == "B":
+			elif direction == "B":
 				print("You moved backwards")
 				print(f'{self.location}')
 		else:
@@ -84,7 +123,14 @@ What would you like to do?\n''')
 						self.allowed_movement.remove('P')
 						print('You picked up the map!')
 					elif move == 'R':
+						direction = move
 						self.allowed_movement.remove('P')
+						self.allowed_movement.remove('R')
+						self.allowed_movement.append('L')
+						self.allowed_movement.append('R')
+						self.allowed_movement.append('F')
+						self.location += MOVEMENT[direction]
+						print(f'You moved right\n{self.location}')
 						break
 					else:
 						print("Not a valid action")
@@ -93,10 +139,193 @@ What would you like to do?\n''')
 
 		if all(self.location == (0,-1,0)):
 			self.allowed_movement.append('L')
+			if 'riddle' in self.RoomItem:
+				self.allowed_movement.append('P')
+				move = None
+				while move != 'P'and'L':
+					print(f'{self.allowed_movement}')
+					smove = input('''You found an old riddle.
+What would you like to do?\n''')
+					move = smove.upper()
+				
+					if move == 'P':
+						self.player_inventory.append('riddle')
+						self.RoomItem.remove('riddle')
+						self.allowed_movement.remove('P')
+						print('You picked up the riddle!')
+					elif move == 'L':
+						direction = move
+						self.allowed_movement.remove('P')
+						self.allowed_movement.append('R')
+						self.allowed_movement.append('F')
+						self.location += MOVEMENT[direction]
+						print(f'You moved left\n{self.location}')
+						
+						break
+					else:
+						print("Not a valid action")
+
 		if all(self.location == (1,0,0)):
 			self.allowed_movement.append('F')
-			print("yay")
-			quit()
+			self.allowed_movement.append('B')
+		if all(self.location == (2,0,0)):
+			self.allowed_movement.append('L')
+			self.allowed_movement.append('R')
+			self.allowed_movement.append('B')
+			if 'flash light' in self.RoomItem:
+				self.allowed_movement.append('P')
+				move = None
+				while move != 'P'and'L'and'R'and'B':
+					print(f'{self.allowed_movement}')
+					smove = input('''You found a flash light.
+What would you like to do?\n''')
+					move = smove.upper()
+					if move == 'P':
+						self.player_inventory.append('flash light')
+						self.RoomItem.remove('flash light')
+						self.allowed_movement.remove('P')
+						print('You picked up the flash light!')
+					elif move == 'L':
+						direction = move
+						self.allowed_movement.remove('R')
+						self.allowed_movement.remove('L')
+						self.allowed_movement.remove('P')
+						self.allowed_movement.remove('B')
+						self.location += MOVEMENT[direction]
+						print(f'You moved left\n{self.location}')
+						
+						break
+					elif move == 'R':
+						direction = move
+						self.allowed_movement = []
+						
+						self.location += MOVEMENT[direction]
+						print(f'You moved left\n{self.location}')
+						
+						break
+					elif move == 'B':
+						direction = move
+						self.allowed_movement.remove('P')
+						self.allowed_movement.remove('R')
+						self.allowed_movement.append('F')
+						self.allowed_movement.remove('L')
+						self.allowed_movement.remove('B')
+						self.allowed_movement.append('B')
+						self.location += MOVEMENT[direction]
+						print(f'You moved left\n{self.location}')
+
+					else:
+
+						print("Not a valid action")
+		if all(self.location == (2,1,0)):
+			self.allowed_movement.append('R')
+			self.allowed_movement.append('F')
+		if all(self.location == (2,-1,0)):
+			self.allowed_movement.append('L')
+			self.allowed_movement.append('R')
+		if all(self.location == (2,-2,0)):
+			self.allowed_movement.append('L')
+			self.allowed_movement.append('F')
+		if all(self.location == (3,1,0)):
+			self.allowed_movement.append('F')
+			self.allowed_movement.append('B')
+		if all(self.location == (3,-2,0)):
+			self.allowed_movement.append('B')
+			if 'protective amulet' in self.RoomItem:
+				self.allowed_movement.append('P')
+				move = None
+				while move != 'P'and'B':
+					print(f'{self.allowed_movement}')
+					smove = input('''You found a protective amulet.
+What would you like to do?\n''')
+					move = smove.upper()
+					if move == 'P':
+						self.player_inventory.append('protective amulet')
+						self.RoomItem.remove('protective amulet')
+						self.allowed_movement.remove('P')
+						print('You picked up the protective amulet!')
+					elif move == 'B':
+						direction = move
+						self.allowed_movement.remove('R')
+						self.allowed_movement.remove('L')
+						self.allowed_movement.remove('P')
+						self.allowed_movement.remove('B')
+						self.location += MOVEMENT[direction]
+						print(f'You moved backwards\n{self.location}')
+					else:
+						print("Not a valid action")
+		if all(self.location == (4,0,0)):
+			self.allowed_movement.append('L')
+			
+			if 'tree monster' in self.animals:
+				move = input("Oh no!\n A tree monster. (fight/run)\n")
+				if move == 'run':
+					print("You ran all the way back to the beach")
+					
+					self.allowed_movement.append('R')
+					self.allowed_movement.append('F')
+					self.location = array([0,0,0])
+				elif move == 'fight':
+					item = input("What item do you want to use\n")
+					if item == 'torch':
+						print('You torched the tree monster and killed it. Good job!')
+					elif item == 'protective amulet':
+						if 'protective amulet' in self.player_inventory:
+							print('You used the protective amulet and lived but you broke the protective amulet.')
+							self.player_inventory.remove('protective amulet')
+							self.animal.remove('tree monster')
+						else:
+							print("You don't have that item. You died")
+							
+							option = input("Do you want to play again? (yes/no)\n")
+							if option == 'yes':
+								
+								self.allowed_movement.append('R')
+								self.allowed_movement.append('F')
+								self.RoomItem = ['map','note','riddle','protective amulet','flash light']
+								self.animals = ['tree monster']
+								self.player_inventory = []
+								self.location = array([0,0,0])
+							elif option == 'no':
+								print("Goodbye")
+								quit()
+							else:
+								print("Not a valid input")
+					else:
+						print("wrong item. You died")
+						option = input("Do you want to play again? (yes/no)\n")
+						if option == 'yes':
+							
+							self.allowed_movement.append('R')
+							self.allowed_movement.append('F')
+							
+							self.RoomItem = ['map','note','riddle','protective amulet','flash light','torch']
+							self.animals = ['tree monster']
+							self.player_inventory = []
+							self.location = array([0,0,0])
+						
+						elif option == 'no':
+							print("Goodbye")
+							quit()
+						else:
+							print("Not a valid input")
+		if all(self.location == (4,1,0)):
+			self.allowed_movement.append('L')
+			self.allowed_movement.append('R')
+			self.allowed_movement.append('B')
+		
+		if all(self.location == (4,2,0)):
+			self.allowed_movement.append('L')
+			self.allowed_movement.append('R')
+		if all(self.location == (4,3,0)):
+			self.allowed_movement.append('L')
+			self.allowed_movement.append('R')
+		if all(self.location == (4,4,0)):
+			self.allowed_movement.append('R')
+			self.allowed_movement.append('F')
+
+
+			
 		
 
 		
--- Test.py
+++ Test.py
@@ -1,58 +1,19 @@
-class Begin():
-	def __init__(self):
-		self.play_game = False
-	def start_game(self):
-		self.play_game = input("Would you like to play my game? (yes/no) \n")
-		if self.play_game == 'yes':
-			print('Welcome to The Lost Island')
-		elif self.play_game == 'no':
-			print("Ok, maybe next time.")
-			quit()
-		else:
-			print("Invalid input. Please type 'yes' or 'no'.")
-			self.start_game()
-game = Begin()
-game.start_game()
-def player_details():
-		
-		player_name = input("Before we begin explorer. What is your name?\n")
-		print(f"Welcome {player_name}, your adventure awaits!")
-		return player_name
-	def welcome():
-		print('''You are a world-famous explorer on a flight across the Pacific Ocean when your plane suddenly experiences a malfunction. \nThe last thing you remember is blacking out during the crash. 
-	You wake up on the beach of an island. You have no idea how you got there or where anyone else is.
-	You will need to explore the island to find the necesaryy materials to escape. Be careful, who knows what kinds of things are on the island.''')
-	def menu1():
-		print('''	Game menu
-Q - quit game
-S - save game
-M - menu
-U - up
-D - down
-L - left
-R - right
-F - forward
-B - backward
-O - use item
-P - pick up item
-I - inventory\n''')
-
-	def menu():
-		print('This is your game menu')
-		print('''	Game menu
-Q - quit game
-S - save game
-M - menu
-U - up
-D - down
-L - left
-R - right
-F - forward
-B - backward
-O - use item
-P - pick up item
-I - inventory\n''')
-		y = input("Are you ready to begin? (yes/no)\n")
-		if y == 'no':
-			print('Goodbye')
-			quit()
+print('''
+                         _
+             _   _   _  |_|	
+           _|_|_|_|_|_| |_|	
+          |_|_|_|_|_|_|_|_|_ _ _
+          |_| |_|   |_|_|_|_|_|_|  _        
+                   _ _ _ _ _ _|_|_|_|_
+               _ _|_|_|_|_|_|_|_|_|_|_|
+     _ 	      |_|_|_|       |_|   |_|
+    |_|_ _ _ _ _ _|_|_  
+    |_|_|_|_|_|_|_|_|_|	
+ _ _ _|_|_	
+|_|_|_|_|_|		
+  |_|_ _ _ _
+  |_|_|_|_|_|  _
+	|_|_ _|_|
+	|_|_|_|_|
+	 _|_|_
+	|_|_|_|''')
